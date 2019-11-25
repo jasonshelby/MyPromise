fef9d02fa495cb5cae0520d1f1e14d83695d8785
jason <122230606@qq.com>
2019-11-25 10:41:25 +0800

    为私有变量的实现做准备

--- mine.js
+++ mine.js
@@ -11,51 +11,65 @@ const nextTick = typeof setImmediate !== 'undefined' ? (task) => {
   setTimeout(task)
 }
 
-function MyPromise(excutor) {
-  this.val = null
-  this.state = PENDING
-  this.deferFuncs = {
-    y: noop,
-    n: noop
+const MyPromise = (function () {
+  // 为后续实现私有变量做准备
+  // const val = new WeakMap()
+  class MyPromise {
+    constructor(excutor) {
+      this.val = null
+      this.state = PENDING
+      this.deferFuncs = {
+        y: noop,
+        n: noop
+      }
+      excutor(this.relsove.bind(this))
+    }
+
+    relsove(output) {
+      // log(output)
+      nextTick(() => {
+        // then中return的值为promise，用.then的方式将这个peomise的值“接住”
+        if (output instanceof MyPromise) {
+          output.then(lastOutput => {
+            // 当前promise的value值就是下一个then的输入值
+            this.val = lastOutput
+            this.deferFuncs.y(this.val)
+          })
+        } else {
+          this.val = output
+          this.deferFuncs.y(this.val) // next input
+        }
+      })
+    }
+
+    then(bindFuncY, bindFuncN) {
+      // 如果then的参数不是函数，需要将值透传，所以返回原有参数
+      bindFuncY = typeof bindFuncY === 'function' ? bindFuncY : v => v
+      bindFuncN = typeof bindFuncN === 'function' ? bindFuncN : v => v
+
+      return new MyPromise((resolve, reject) => {
+        this.deferFuncs.y = (input) => {
+          const output = bindFuncY(input)
+          // log(output)
+          resolve(output)
+        }
+        this.deferFuncs.n = () => {
+          const errorReson = bindFuncN()
+          this.val = errorReson
+          reject(errorReson)
+        }
+      })
+    }
+
   }
-  excutor(this.relsove.bind(this))
-}
+  return MyPromise
+})()
 
 // 本质是对结果进行处理（存起来，并调用下一个promise）
-MyPromise.prototype.relsove = function (output) {
-  // log(output)
-  nextTick(() => {
-    // then中return的值为promise，用.then的方式将这个peomise的值“接住”
-    if (output instanceof MyPromise) {
-      output.then(lastOutput => {
-        this.val = lastOutput
-        this.deferFuncs.y(this.val)
-      })
-    } else {
-      this.val = output
-      this.deferFuncs.y(this.val) // next input
-    }
-  })
-}
+// MyPromise.prototype.
+// }
 
-MyPromise.prototype.then = function (bindFuncY, bindFuncN) {
-  // 如果then的参数不是函数，需要将值透传，所以返回原有参数
-  bindFuncY = typeof bindFuncY === 'function' ? bindFuncY : v => v
-  bindFuncN = typeof bindFuncN === 'function' ? bindFuncN : v => v
-  
-  return new MyPromise((resolve, reject) => {
-    this.deferFuncs.y = (input) => {
-      const output = bindFuncY(input)
-      // log(output)
-      resolve(output)
-    }
-    this.deferFuncs.n = () => {
-      const errorReson = bindFuncN()
-      this.val = errorReson
-      reject(errorReson)
-    }
-  })
-}
+// MyPromise.prototype.
 
 var demo = new MyPromise((resolve, reject) => {
   log('01')
