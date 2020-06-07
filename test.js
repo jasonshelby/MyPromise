e95ca67318a95facc183ab2dd3124778abca48af
name <eamil-address>
2020-06-07 23:43:42 +0800

    修复 2.1.2

--- test.js
+++ test.js
@@ -1,23 +1,8 @@
 const PromiseA = require('./index')
 
 log = console.log
-var demo = new PromiseA((resolve, reject) => {
-  const r = 0
+var demo = new Promise((resolve, reject) => {
   r = 1
   resolve(1)
-
-}).then(1, 2).then(() => {
-  console.log(11)
-  return new PromiseA((resolve) => {
-    console.log('y')
-    resolve('y4')
-  })
-}, () => {
-  console.log(22)
-  return new PromiseA((resolve) => {
-    console.log('n')
-    resolve('n4')
-  })
-}).then((val) => {
-  console.log(val)
+  reject(2)
 })
